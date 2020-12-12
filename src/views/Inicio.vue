42693f17999ecfe6c004f933e9684b02d8c6b04f
Daniela Luiza <danii.bnu@hotmail.com>
2020-12-12 16:59:08 -0300

    new inicio

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -6,6 +6,7 @@
       src="https://img.elo7.com.br/product/zoom/22E4D0E/painel-papai-noel-natal-frete-gratis-painel-digital.jpg">
     </v-img>
     <p>Nossa missão é conectar vocês interessados em presentar as crianças do Brasil, contribuindo para um natal mais feliz e igualitário.</p>
+    <p> O natal é momento de esperança, amor e solidariedade. Por isso temo a opção de fazer mais de uma boa ação. Adote a carta de uma criança carente e adote um melhor amigo de quatro patas! </p>
   </v-container>
 </template>
 <script>
