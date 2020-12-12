049c02dd23b1e0855ceeaf9a2629bf4d5d51649e
Daniela Luiza <danii.bnu@hotmail.com>
2020-12-12 12:38:30 -0300

    App.vue and Router fixed

---
+++ src/components/shared/AppTopBar.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-card class="apptopbar">
+    <v-app-bar app
+      absolute
+      color="#43a047"
+      dark
+      shrink-on-scroll
+      prominent
+      src="https://www.thebuckingham.com.au/wp-content/uploads/2018/11/christmas-1.jpg"
+      fade-img-on-scroll
+      scroll-target="#scrolling-techniques-5"
+      scroll-threshold="500"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(55,236,186,.7), rgba(255,0,0,.7)"
+        ></v-img>
+      </template>
+
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Natal Solidário</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+  
+    </v-app-bar>
+    
+      <v-container style="height: 1500px;"></v-container>
+       <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="red--text text--accent-4"
+        >
+          <router-link to="/">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Início</v-list-item-title>
+            </v-list-item>
+          </router-link>
+          <router-link to="/">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Usuário</v-list-item-title>
+            </v-list-item>
+          </router-link>
+          <router-link to="/ListaCartas">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-gift</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Adote uma Carta</v-list-item-title>
+            </v-list-item>
+          </router-link>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "AppTopBar",
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+};
+</script>
+
+<style scoped>
+    .apptopbar a{
+        text-decoration: none;
+    }
+</style>
